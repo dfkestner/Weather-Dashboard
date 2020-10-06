a7990c66aacfba8c014e764148e450128a7403be
Tea Flores-Kestner <dfkestner@gmail.com>
2020-10-05 21:05:55 -0400

    reduced number of ajax calls to pull five day and uv data from single lat and lon call, added icons and some icon styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
             function getUV(x, y) {
 
                 var uvKey = "6169fd357ae3eb07682f558919fa208b";
-                var uvURL = "https://api.openweathermap.org/data/2.5/uvi?lat=" + x + "&lon=" + y + "&appid=" + uvKey;
+                var uvURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + x + "&lon=" + y + "&exclude=minutely,hourly&units=imperial&appid=" + uvKey;
 
                 $.ajax({
                     url: uvURL,
@@ -21,17 +21,47 @@ $(document).ready(function() {
                 }).then(function(response) {
                     console.log(response);
 
-                    var UV = $("<p>").text("UV Index: " + response.value)
+                    var UV = $("<p>").text("UV Index: " + response.daily[0].uvi)
                     $("#currentStats").append(UV)
+
+                    $("#fiveDay").text("Five Day Forecast");
+
+                    $("#day1").text(moment().add(1, "day").format('L'));
+                    $("#icon1").attr("src", "http://openweathermap.org/img/wn/" + response.daily[0].weather[0].icon + "@2x.png");
+                    var temp1 = $("<p>").text("Temp: " + Math.ceil(response.daily[0].temp.day) + "\xB0" + "F");
+                    var humid1 = $("<p>").text("Humidity: " + (response.daily[0].humidity) + "%");
+                    $("#day1").append(temp1, humid1);
+                    
+                    $("#day2").text(moment().add(2, "day").format('L'));
+                    $("#icon2").attr("src", "http://openweathermap.org/img/wn/" + response.daily[1].weather[0].icon + "@2x.png");
+                    var temp2 = $("<p>").text("Temp: " + Math.ceil(response.daily[1].temp.day) + "\xB0" + "F");
+                    var humid2 = $("<p>").text("Humidity: " + (response.daily[1].humidity) + "%");
+                    $("#day2").append(temp2, humid2);
+
+                    $("#day3").text(moment().add(3, "day").format('L'));
+                    $("#icon3").attr("src", "http://openweathermap.org/img/wn/" + response.daily[2].weather[0].icon + "@2x.png");
+                    var temp3 = $("<p>").text("Temp: " + Math.ceil(response.daily[2].temp.day) + "\xB0" + "F");
+                    var humid3 = $("<p>").text("Humidity: " + (response.daily[2].humidity) + "%");
+                    $("#day3").append(temp3, humid3);
+
+                    $("#day4").text(moment().add(4, "day").format('L'));
+                    $("#icon4").attr("src", "http://openweathermap.org/img/wn/" + response.daily[3].weather[0].icon + "@2x.png");
+                    var temp4 = $("<p>").text("Temp: " + Math.ceil(response.daily[3].temp.day) + "\xB0" + "F");
+                    var humid4 = $("<p>").text("Humidity: " + (response.daily[3].humidity) + "%");
+                    $("#day4").append(temp4, humid4);
+
+                    $("#day5").text(moment().add(5, "day").format('L')).addClass("col");
+                    $("#icon5").attr("src", "http://openweathermap.org/img/wn/" + response.daily[4].weather[0].icon + "@2x.png");
+                    var temp5 = $("<p>").text("Temp: " + Math.ceil(response.daily[4].temp.day) + "\xB0" + "F");
+                    var humid5 = $("<p>").text("Humidity: " + (response.daily[4].humidity) + "%");
+                    $("#day5").append(temp5, humid5);
                 })
             }
 
             console.log(response);
 
-            // +  "<h2 class='daily'>" + response.name + " (" + startDate + ")" + "&nbsp" + "<img src='" + iconUrl  + "'>" + "</h2>"
-
-            $("#currentWeather").text((response.name) + ", " + (moment().format("MMMM Do, YYYY")))
-            // var iconURL = "http://openweathermap.org/img/w" + response.weather[0].icon + ".png";
+            $("#currentWeather").text(response.name + ", " + moment().format("MMMM Do, YYYY"));
+            $("#icon").attr("src", "http://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png");
 
             var thisCity = response.name;
             var temp = $("<p>").text("Temperature: " + Math.ceil(response.main.temp) + "\xB0" + "F");
@@ -39,7 +69,7 @@ $(document).ready(function() {
             var windSpeed = $("<p>").text("Wind Speed: " + Math.ceil(response.wind.speed) + " MPH");
             $("#currentStats").append(temp, humid, windSpeed);
 
-            var recentCities = $("<li>").text(response.name);
+            var recentCities = $("<button>").text(thisCity).addClass("btn btn-outline-secondary").val(thisCity);
             $(".list-group").append(recentCities);
 
             var lat = response.coord.lat;
@@ -47,75 +77,17 @@ $(document).ready(function() {
 
             getUV(lat, lon)
 
-            get5day(thisCity)
-        });
-        
-        function get5day(forecast) {
+            var cityList = JSON.parse(localStorage.getItem("addCity")) || [];
 
-            var thisKey = "6169fd357ae3eb07682f558919fa208b";
-            var thisURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + forecast + "&units=imperial&appid=" + thisKey
+            var addCity = response.name;
 
-            $.ajax({
-                url: thisURL,
-                method: "GET"
-            }).then(function(response) {
-                console.log(response);
-                
-                $("#fiveDay").text("Five Day Forecast");
-                var date1 = $("<p>").text(moment(response.list[0].dt_txt).format());
-                $("#fiveDayStats").append(date1)
-                var temp1 = $("<p>").text("Temperature: " + Math.ceil(response.list[0].main.temp) + "\xB0" + "F");
-                $("#fiveDayStats").append(temp1);
-                var humid1 = $("<p>").text("Humidity: " + (response.list[0].main.humidity) + "%");
-                $("#fiveDayStats").append(humid1);
-                // var icon1 = response.list[0].weather[0].icon;
-                console.log(response.list[0].main.temp);
-                console.log(response.list[0].main.humidity);
-                console.log(moment(response.list[0].dt_txt).format());
-                
-                var date2 = $("<p>").text(moment(response.list[1].dt_txt).format('MMMM Do, YYYY'));
-                $("#fiveDayStats").append(date2)
-                var temp2 = $("<p>").text("Temperature: " + Math.ceil(response.list[1].main.temp) + "\xB0" + "F");
-                $("#fiveDayStats").append(temp2);
-                var humid2 = $("<p>").text("Humidity: " + (response.list[1].main.humidity) + "%");
-                $("#fiveDayStats").append(humid2);
-                console.log(response.list[1].main.temp);
-                console.log(response.list[1].main.humidity);
-                console.log(moment(response.list[1].dt_txt).format('MMMM Do, YYYY'))
-
-                var date3 = $("<p>").text(moment(response.list[2].dt_txt).format('MMMM Do, YYYY'));
-                $("#fiveDayStats").append(date3)
-                var temp3 = $("<p>").text("Temperature: " + Math.ceil(response.list[2].main.temp) + "\xB0" + "F");
-                $("#fiveDayStats").append(temp3);
-                var humid3 = $("<p>").text("Humidity: " + (response.list[2].main.humidity) + "%");
-                $("#fiveDayStats").append(humid3);
-                console.log(response.list[2].main.temp);
-                console.log(response.list[2].main.humidity);
-                console.log(moment(response.list[2].dt_txt).format('MMMM Do, YYYY'))
-
-                var date4 = $("<p>").text(moment(response.list[3].dt_txt).format('MMMM Do, YYYY'));
-                $("#fiveDayStats").append(date4)
-                var temp4 = $("<p>").text("Temperature: " + Math.ceil(response.list[3].main.temp) + "\xB0" + "F");
-                $("#fiveDayStats").append(temp4);
-                var humid4 = $("<p>").text("Humidity: " + (response.list[3].main.humidity) + "%");
-                $("#fiveDayStats").append(humid4);
-                console.log(response.list[3].main.temp);
-                console.log(response.list[3].main.humidity);
-                console.log(moment(response.list[3].dt_txt).format('MMMM Do, YYYY'))
-
-                var date5 = $("<p>").text(moment(response.list[4].dt_txt).format('MMMM Do, YYYY'));
-                $("#fiveDayStats").append(date5)
-                var temp5 = $("<p>").text("Temperature: " + Math.ceil(response.list[4].main.temp) + "\xB0" + "F");
-                $("#fiveDayStats").append(temp5);
-                var humid5 = $("<p>").text("Humidity: " + (response.list[4].main.humidity) + "%");
-                $("#fiveDayStats").append(humid5);
-                console.log(response.list[4].main.temp);
-                console.log(response.list[4].main.humidity);
-                console.log(moment(response.list[4].dt_txt).format('MMMM Do, YYYY'))
-            })
-        }
+            if([cityList].indexOf(addCity) == -1) {
+                [cityList].push(addCity)
+                window.localStorage.setItem("addCity", JSON.stringify(addCity));
+            }
+        }); 
     }
-    
+
     $("#searchButton").on("click", function(event) {
         event.preventDefault();
 
